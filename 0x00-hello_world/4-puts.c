ef618b74fca5e4744b7ae7e02cf7c1867ba4c365
NeoNkomo12 <neomolenkomo@gmail.com>
2023-07-14 17:45:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.h
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Successible execution)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
