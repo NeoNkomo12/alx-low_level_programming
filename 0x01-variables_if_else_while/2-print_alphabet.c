7ad1fbbc817bcff33ca28568b4eb91ff73714740
NeoNkomo12 <neomolenkomo@gmail.com>
2023-07-17 19:26:50 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Access point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char[26]="abcdefghijkllmnopqrstuvwxyz", int i;
+
+	for (i = 0; i < 26, i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar("\n");
+	return(0);
+}
